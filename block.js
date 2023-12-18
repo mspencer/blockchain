914b27d12ff8786823c4ce841f1afe8a2b8d0905
mintra <mintrat@gmail.com>
2023-12-18 13:52:56 +0000

    end Section 2: done Block class

--- block.js
+++ block.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+const { GENESIS_DATA } = require("./config");
+const cryptoHash = require("./cryptohash");
+
+>>>>>>> ff578f1 (Section 2: individual block in Block class)
 class Block {
     constructor({ timestamp, lastHash, hash, data }) {
         this.timestamp = timestamp;
@@ -5,6 +11,7 @@ class Block {
         this.hash = hash;
         this.data = data;
     }
+<<<<<<< HEAD
 }
 
 const block1 = new Block({
@@ -15,3 +22,24 @@ const block1 = new Block({
 });
 
 console.log('block1: ', block1);
+=======
+
+    static genesis() {
+        // "this" refers to the Block class itself
+        return new this(GENESIS_DATA);
+    }
+    
+    static mineBlock({ lastBlock, data }) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        return new this({
+            timestamp,
+            lastHash,
+            data,
+            hash: cryptoHash(timestamp, lastHash, data)
+        });
+    }
+}
+
+module.exports = Block;
+>>>>>>> ff578f1 (Section 2: individual block in Block class)
