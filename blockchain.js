ac9be3eacc891fecaabf7ec5044a3cc599f8d1a9
mintra <mintrat@gmail.com>
2023-12-23 01:04:34 +0000

    Implement proof of work. Adjust the difficulty so the blocks are mined at the specific mine rate. Look at proof of work systems (hexadecimal vs binary) against the pre-config mine rate. Prevent difficulty jump.

--- blockchain.js
+++ blockchain.js
@@ -19,11 +19,17 @@ class Blockchain {
         if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
 
         for(let i=1; i<chain.length; i++) {
-            const { timestamp, lastHash, hash, data } = chain[i];
+            const { timestamp, nonce, difficulty, lastHash, hash, data } = chain[i];
             const actualLastHash = chain[i-1].hash;
+            const lastDifficulty = chain[i-1].difficulty;
+
             if(lastHash !== actualLastHash) return false;
-            const validateHash = cryptoHash(timestamp, lastHash, data);
+            
+            const validateHash = cryptoHash(timestamp, nonce, difficulty, lastHash, data);
+            
             if(hash !== validateHash) return false;
+
+            if(Math.abs(lastDifficulty - difficulty) > 1) return false;
         }
         return true;
     }
